3af3ed3fa34977caa3823a544f5b2d4ac2ee8215
panchaldeep009 <panchaldeep009@gmail.com>
2018-10-23 17:11:22 -0400

    Sorting Genres

--- js/dataMaping.js
+++ js/dataMaping.js
@@ -5,9 +5,17 @@
     MovieData.forEach(thisMovie => {
         if(thisMovie.Response != "False"){
             thisMovie.Genre.split(', ').forEach(thisGenre =>{
-                if(!Genres.includes(thisGenre)){ Genres.push(thisGenre); }
+                if (Genres.filter(g => g.Name === thisGenre).length <= 0) {
+                    Genres.push({"Name": thisGenre, "count": 1});
+                } else {
+                    Genres.map(g => { if(g.Name === thisGenre){ g.count++; } });
+                }
             });
         }
     });
+    
+    // Sort Genres By it's coming
+    Genres = Genres.sort(function(a,b) { return b.count - a.count });
+
     console.log(Genres);
 })();
